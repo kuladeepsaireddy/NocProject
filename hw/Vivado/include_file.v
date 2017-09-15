3ea23c5230abbd607e5541932e270e6dbd0fa1bf
kuladeep <kuladeepsai123@gmail.com>
2017-09-15 21:06:25 +0530

    Final files

--- hw/Vivado/include_file.v
+++ hw/Vivado/include_file.v
@@ -1,9 +1,9 @@
-`define X 2
-`define Y 2
+`define X 8
+`define Y 8
 `define data_width 256
 `define total_width (`x_size+`y_size+`pck_num+`data_width)
 `define sw_no `X*`Y  //number of swithces 
-`define x_size 1
-`define y_size 1
-`define pck_num 2
+`define x_size 3
+`define y_size 3
+`define pck_num 6
 `define iter `data_width/8
